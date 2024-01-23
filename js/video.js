7ee431a59d8a72e4666e380de95fc7a3c37006f6
Anh Duy <anonymous4dev2hide@gmail.com>
2024-01-23 22:47:07 +0700

    update result page

--- js/video.js
+++ js/video.js
@@ -2,57 +2,35 @@ const videosList = [
     {
         video: 'https://www.youtube.com/embed/MX8g-8NXx7w',
         title: 'Red cars',
-        prompt: `<p style="font-size: 20px; text-align: center;">Tracking prompt: 
-        <span style="font-weight: bold; color: green;">"car</span>
-        <span style="font-family: 'Courier New', monospace; font-weight: bold;">&nbsp;&lt;include&gt;</span>
-        <span style="font-weight: bold; color: orange;">&nbsp;red color</span>
-        <span style="font-family: 'Courier New', monospace; font-weight: bold;">&nbsp;&lt;exclude&gt;</span>
-        <span style="font-weight: bold;  color: crimson;">&nbsp;yellow, blue, black, white color"</span>
+        prompt: `<p style="font-family: 'Courier New', monospace; font-weight: bold; text-align: center;">"Track <span style="color: orange;">red color</span> <span style="font-weight: bold; color: green;">car</span> while exclude
+        <span style="font-weight: bold;  color: crimson;">yellow, blue, black, white color</span> <span style="font-weight: bold; color: green;">car</span>"
         </p>`
     },
     {
         video: 'https://www.youtube.com/embed/H7nVN4VRhbk',
         title: 'Yellow cars',
-        prompt: `<p style="font-size: 20px; text-align: center;">Tracking prompt: 
-        <span style="font-weight: bold; color: green;">"car</span>
-        <span style="font-family: 'Courier New', monospace; font-weight: bold;">&nbsp;&lt;include&gt;</span>
-        <span style="font-weight: bold; color: orange;">&nbsp;yellow color</span>
-        <span style="font-family: 'Courier New', monospace; font-weight: bold;">&nbsp;&lt;exclude&gt;</span>
-        <span style="font-weight: bold; color: crimson;">&nbsp;red, blue, black, white color"</span>
+        prompt: `<p style="font-family: 'Courier New', monospace; font-weight: bold; text-align: center;">"Track <span style="color: orange;">yellow color</span> <span style="font-weight: bold; color: green;">car</span> while exclude
+        <span style="font-weight: bold;  color: crimson;">red, blue, black, white color</span> <span style="font-weight: bold; color: green;">car</span>"
         </p>`
     },
     {
         video: 'https://www.youtube.com/embed/E7mSlziENQ4',
         title: 'Dark blue cars',
-        prompt: `<p style="font-size: 20px; text-align: center;">Tracking prompt: 
-        <span style="font-weight: bold; color: green;">"car</span>
-        <span style="font-family: 'Courier New', monospace; font-weight: bold;">&nbsp;&lt;include&gt;</span>
-        <span style="font-weight: bold; color: orange;">&nbsp;dark blue car</span>
-        <span style="font-family: 'Courier New', monospace; font-weight: bold;">&nbsp;&lt;exclude&gt;</span>
-        <span style="font-weight: bold; color: crimson;">&nbsp;red, yellow, black, white"</span>
+        prompt: `<p style="font-family: 'Courier New', monospace; font-weight: bold; text-align: center;">"Track <span style="color: orange;">dark blue color</span> <span style="font-weight: bold; color: green;">car</span> while exclude
+        <span style="font-weight: bold;  color: crimson;">red, yellow, black, white color</span> <span style="font-weight: bold; color: green;">car</span>"
         </p>`
     },
     {
         video: 'https://www.youtube.com/embed/6Yte9nL2d0k',
         title: 'Airplanes',
-        prompt: `<p style="font-size: 22px; text-align: center;">Tracking prompt: 
-        <span style="font-weight: bold; color: green;">"airplane</span>
-        <span style="font-family: 'Courier New', monospace; font-weight: bold;">&nbsp;&lt;include&gt;</span>
-        <span style="font-weight: bold; color: orange;">&nbsp;metal shell</span>
-        <span style="font-family: 'Courier New', monospace; font-weight: bold;">&nbsp;&lt;exclude&gt;</span>
-        <span style="font-weight: bold; color: crimson;">&nbsp;{none}"</span>
-        </p>`
+        prompt: `<p style="font-family: 'Courier New', monospace; font-weight: bold; text-align: center;">"Track <span style="color: orange;">metal shell</span> <span style="font-weight: bold; color: green;">airplane</span>"
+    </p>`
     },
     {
         video: 'https://www.youtube.com/embed/Eh5HJUSL9NE',
         title: 'Helicopters',
-        prompt: `<p style="font-size: 22px; text-align: center;">Tracking prompt: 
-        <span style="font-weight: bold; color: green;">"helicopter</span>
-        <span style="font-family: 'Courier New', monospace; font-weight: bold;">&nbsp;&lt;include&gt;</span>
-        <span style="font-weight: bold; color: orange;">&nbsp;flying; rotor blades</span>
-        <span style="font-family: 'Courier New', monospace; font-weight: bold;">&nbsp;&lt;exclude&gt;</span>
-        <span style="font-weight: bold; color: crimson;">&nbsp;{none}"</span>
-        </p>`
+        prompt: `<p style="font-family: 'Courier New', monospace; font-weight: bold; text-align: center;">"Track <span style="color: orange;">flying</span> <span style="font-weight: bold; color: green;">helicopter</span>"
+    </p>`
     },
     ]
     
