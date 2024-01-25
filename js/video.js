4e0e541823519057062b09f0a49a70b7609faffe
Anh Duy <anonymous4dev2hide@gmail.com>
2024-01-25 23:39:31 +0700

    fix iframe display result page

--- js/video.js
+++ js/video.js
@@ -1,6 +1,7 @@
 const videosList = [
     {
         video: 'https://www.youtube.com/embed/MX8g-8NXx7w',
+        thumbnail: 'https://img.youtube.com/vi/MX8g-8NXx7w/maxresdefault.jpg',
         title: 'Red cars',
         prompt: `<p style="font-family: 'Courier New', monospace; font-weight: bold; text-align: center;">"Track <span style="color: orange;">red color</span> <span style="font-weight: bold; color: green;">car</span> while exclude
         <span style="font-weight: bold;  color: crimson;">yellow, blue, black, white color</span> <span style="font-weight: bold; color: green;">car</span>"
@@ -8,6 +9,7 @@ const videosList = [
     },
     {
         video: 'https://www.youtube.com/embed/H7nVN4VRhbk',
+        thumbnail: 'https://img.youtube.com/vi/H7nVN4VRhbk/maxresdefault.jpg',
         title: 'Yellow cars',
         prompt: `<p style="font-family: 'Courier New', monospace; font-weight: bold; text-align: center;">"Track <span style="color: orange;">yellow color</span> <span style="font-weight: bold; color: green;">car</span> while exclude
         <span style="font-weight: bold;  color: crimson;">red, blue, black, white color</span> <span style="font-weight: bold; color: green;">car</span>"
@@ -15,6 +17,7 @@ const videosList = [
     },
     {
         video: 'https://www.youtube.com/embed/E7mSlziENQ4',
+        thumbnail: 'https://img.youtube.com/vi/E7mSlziENQ4/maxresdefault.jpg',
         title: 'Dark blue cars',
         prompt: `<p style="font-family: 'Courier New', monospace; font-weight: bold; text-align: center;">"Track <span style="color: orange;">dark blue color</span> <span style="font-weight: bold; color: green;">car</span> while exclude
         <span style="font-weight: bold;  color: crimson;">red, yellow, black, white color</span> <span style="font-weight: bold; color: green;">car</span>"
@@ -22,12 +25,14 @@ const videosList = [
     },
     {
         video: 'https://www.youtube.com/embed/6Yte9nL2d0k',
+        thumbnail: 'https://img.youtube.com/vi/6Yte9nL2d0k/maxresdefault.jpg',
         title: 'Airplanes',
         prompt: `<p style="font-family: 'Courier New', monospace; font-weight: bold; text-align: center;">"Track <span style="color: orange;">metal shell</span> <span style="font-weight: bold; color: green;">airplane</span>"
     </p>`
     },
     {
         video: 'https://www.youtube.com/embed/Eh5HJUSL9NE',
+        thumbnail: 'https://img.youtube.com/vi/Eh5HJUSL9NE/maxresdefault.jpg',
         title: 'Helicopters',
         prompt: `<p style="font-family: 'Courier New', monospace; font-weight: bold; text-align: center;">"Track <span style="color: orange;">flying</span> <span style="font-weight: bold; color: green;">helicopter</span>"
     </p>`
@@ -36,11 +41,12 @@ const videosList = [
     
     const categories = [...new Set(videosList.map((item) => { return item }))]
     document.getElementById('videosList').innerHTML = categories.map((item) => {
-        var { video, title, prompt } = item;
+        var { video, thumbnail, title, prompt } = item;
         return (
             `<div class="list active">
             <div class="list-prompt" hidden>${prompt}</div>
-            <iframe src=${video} class="list-video"></iframe>
+            <iframe src=${video} class="list-video" hidden></iframe>
+            <img src=${thumbnail} class="list-video">
             <div style="width: 100%; height: 100%">
                 <p class="list-title">${title}</p>
             </div>
